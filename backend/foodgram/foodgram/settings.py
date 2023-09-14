44bbfdc79873e63e4e708d03f3266087f641aca6
Garipov Arthur <gam1997.05.31@gmail.com>
2023-09-14 17:50:11 +0300

    исправлен Debug в сеттингах и .env

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -7,7 +7,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-DEBUG = os.getenv('DEBUG', default=True)
+DEBUG = os.getenv('DEBUG', default=False)
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').strip(' "').split(',')
 
